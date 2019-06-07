7b38344bdff3efcc70a4c7c065c4978e57dfee8d
Henry Regis <regislenormandpentagonal@gmail.com>
2019-06-07 06:05:45 +0200

    filling test/server #166501210

---
+++ test/server.js
@@ -0,0 +1,13 @@
+
+const app = require('./../server');
+var request = require('supertest');
+describe('Basic server test configurations for mocha and travis', function(){
+    describe('GET /', function () {
+        it('responds with json', function (done) {
+            request(app)
+                .get('/')
+                .set('Accept', 'application/json')
+                .expect(200, done);
+        });
+    });
+})
